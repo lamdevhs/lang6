869b1fd2fee862deb57403b6580887128afa630d
lamdevhs <lam.dev.hs@gmail.com>
2019-02-12 12:39:19 +0100

    commentaires pour print_automaton

--- print_automaton2.ml
+++ print_automaton2.ml
@@ -1,38 +1,55 @@
 let print_auto : afn -> unit =
   let map = List.map in
   let string_of_char, strlen = String.make 1, String.length in
+  (* x est-il element de la liste ys ? *)
   let is_in ys x = List.exists ((=) x) ys in
+  (* interval a b === [a; a+1; ...; b-1; b] *)
   let rec interval : int -> int -> int list = fun a b ->
     let rec fill result n = if a > n
       then result
       else fill (n :: result) (n-1) in
     fill [] b in
+  (* zip f [a;b;c] [x;y;z] === [f a x; f b y; f c z] *)
   let rec zip
   : 'x 'y 'z. ('x->'y->'z) -> 'x list -> 'y list -> 'z list =
     fun f xs ys -> match (xs, ys) with
     | ([],_) -> []
     | (_,[]) -> []
     | (x::xs, y::ys) -> f x y :: zip f xs ys in
+  (* fuse: fusionne les elements d'une liste en une seule valeur *)
+  (* fuse (+) 0 [1;42;100] === 143 *)
+  (* fuse (^) "" ["hello ", "world"] === "hello world" *)
+  (* fuse f def [a;b;c;d] === f (f (f a b) c) d *)
+  (* fuse f def [] === def *)
+  (* fuse f def [a] === a *)
   let fuse : 'x. ('x -> 'x -> 'x) -> 'x -> 'x list -> 'x =
     fun f default_value -> function
     | [] -> default_value 
     | x :: rest -> List.fold_left f x rest in
+  (* etats accessible a partir d'un etat en lisant une lettre *)
   let transits : etatN -> char -> int list =
     fun state letter ->
       try state.tN letter with
       | Match_failure _ -> [] in
+  (* ensemble d'entiers --> chaine de caracteres :
+    [] --> "" ;; [1] --> "1" ;; [2;3] --> "{2,3}" *)
   let string_of_ints : int list -> string =
     fun ints -> match (map string_of_int ints) with
     | [] -> ""
     | [str] -> str
     | strlist -> let with_comma a b = a ^ "," ^ b
       in "{" ^ (fuse with_comma "" strlist) ^ "}" in
+  (* donne le max de la taille des strings dans un tableau *)
+  (* pour chaque colonne de celui-ci *)
   let get_column_sizes : string list list -> int list =
-    fun table -> fuse (zip max) [] (map (map strlen) table) in
-  let table_of_automaton : afn -> string list list = fun afn ->
+    fun table -> fuse (zip max) []
+      (map (map strlen) table) in
+  let table_of_automaton : afn -> string list list =
+    fun afn ->
     let sigma = afn.sigmaN in
     let sigma_row = "" :: "" :: map string_of_char sigma in
-    let str_transits state c = string_of_ints (transits state c) in
+    let str_transits state c =
+      string_of_ints (transits state c) in
     let make_state_row state_num =
       let col_one =
         if (is_in afn.initN) state_num then "=>" else "" in
@@ -46,7 +63,10 @@ let print_auto : afn -> unit =
     let all_states = interval 1 afn.nN in
     let states_rows = map make_state_row all_states in
     sigma_row :: states_rows in
-  let format_table : int -> string list list -> string list list =
+  (* bourre les cases de la table avec des espaces pour *)
+  (* avoir un affichage regulier. *)
+  let format_table
+  : int -> string list list -> string list list =
     fun margin table ->
       let column_sizes = get_column_sizes table in
       let spaces n = String.make n ' ' in
@@ -55,7 +75,8 @@ let print_auto : afn -> unit =
         let left = diff / 2 in let right = diff - left in
         (spaces left) ^ str ^ (spaces right) in
       map (zip pad_string column_sizes) table in
-  let string_of_table : string -> int -> string list list -> string =
+  let string_of_table
+  : string -> int -> string list list -> string =
     fun sep margin table ->
       let nice_table = format_table margin table in
       let with_sep s1 s2 = s1 ^ sep ^ s2 in
